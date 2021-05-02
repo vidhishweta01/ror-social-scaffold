6c33216b952f82c2e99d531f79dc0e8aeba77186
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-02 11:01:48 +0530

    fixed linter error

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -7,7 +7,7 @@ module PostHelper
 
   def unfriend_post(friend)
     if current_user # rubocop:disable Style/GuardClause
-      link_to 'Unfriend', invitations_destroy_path(invitation_id: Invitation.find_invitation(current_user.id, friend.id))
+      link_to 'Unfriend', invitations_destroy_path(invitation_id: Invitation.find_invitation(current_user.id, friend.id)) # rubocop:disable Layout/LineLength
     end
   end
 end
