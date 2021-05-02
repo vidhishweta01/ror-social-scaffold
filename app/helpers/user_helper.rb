1dca441387ca6ca3fb5ca92981347a39ae9945ed
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-02 10:22:14 +0530

    fixed linter errors

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,13 +1,13 @@
 module UserHelper
   def send_invite
-    if signed_in? && !Invitation.reacted?(current_user.id, @user.id) && current_user != @user
-      link_to 'Send Invitation', invitations_create_path(ids: {id1: current_user.id, id2: @user.id})
+    if signed_in? && !Invitation.reacted?(current_user.id, @user.id) && current_user != @user # rubocop:disable Style/GuardClause
+      link_to 'Send Invitation', invitations_create_path(ids: { id1: current_user.id, id2: @user.id })
     end
   end
 
   def unfriend(friend)
-    if @user == current_user 
-      link_to 'Unfriend',invitations_destroy_path(invitation_id: Invitation.find_invitation(@user.id, friend.id))
+    if @user == current_user # rubocop:disable Style/GuardClause
+      link_to 'Unfriend', invitations_destroy_path(invitation_id: Invitation.find_invitation(@user.id, friend.id))
     end
   end
 
