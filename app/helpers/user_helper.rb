a6e8eb45229a94127f0e0c175de356d38788ebd5
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-09 19:22:25 +0530

    added method in helper and change the view

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,4 +1,10 @@
 module UserHelper
+  def send_invites(user)
+    if signed_in? && !Invitation.reacted?(current_user.id, user.id) && current_user != user # rubocop:disable Style/GuardClause
+      link_to 'Send Invitation', invitations_create_path(ids: { id1: current_user.id, id2: user.id })
+    end
+  end
+
   def send_invite
     if signed_in? && !Invitation.reacted?(current_user.id, @user.id) && current_user != @user # rubocop:disable Style/GuardClause
       link_to 'Send Invitation', invitations_create_path(ids: { id1: current_user.id, id2: @user.id })
@@ -21,6 +27,10 @@ module UserHelper
 
   def reject_invitation(invitation)
     link_to 'Reject', invitations_destroy_path(invitation_id: invitation.id)
+  end
+
+  def post_created
+    Post.all.ordered_by_most_recent.includes(:user)
   end
 
   def mutual_friend(friend)
